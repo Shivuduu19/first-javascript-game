3552163bd869b278a56f53a5478a5a6b7d736dae
shiva kumar <98257507+Shivuduu19@users.noreply.github.com.com>
2023-03-13 17:09:26 +0530

    added play Round function

--- game.js
+++ game.js
@@ -4,3 +4,34 @@ function getComputerChoice(){
     
 return options[choice];
 }
+
+let rock;
+let paper;
+let scissors;
+function result(playerSelection,computerSelection)
+{
+ let yes;
+    if(playerSelection === rock && computerSelection === paper)
+    {
+        result = "You Lose! Paper beats Rock";
+    }else if(playerSelection === scissors && computerSelection === paper)
+    {
+        result= "you Won! Scissors beats paper";
+    } else if(playerSelection=== paper && computerSelection===scissors)
+    {
+        result= "You Lose! scissors beats paper";
+    } else if(playerSelection===paper && computerSelection ===rock)
+    {
+        result= "You Won! paper beats rock";
+    }else if(playerSelection===scissors&& computerSelection===rock)
+    {
+        result= "YOU Lose! rock beats scissors";
+    }else{ 
+        result="You won! rock beats scissors";
+    }
+    return result
+    
+
+}
+
+result(scissors,rock);
